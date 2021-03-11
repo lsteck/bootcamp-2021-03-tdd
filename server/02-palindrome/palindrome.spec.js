b7d905ad1c506f2f758d2374d20ab80678b38405
Larry Steck <lsteck@us.ibm.com>
2021-03-11 12:35:34 -0600

    passes dog

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -4,12 +4,18 @@ describe('the palindrome canary spec', () => {
   });
 });
 
-let isPalindrome = () => {
-  return true;
+let isPalindrome = (word) => {
+  const reversedWord = word.split('').reverse().join('');
+
+  return word === reversedWord;
 };
 
 describe('Given a palindrome function ', () => {
   it('should return true when called with radar', () => {
     expect(isPalindrome('radar')).toBe(true);
+  });
+
+  it('should return false when called with dog', () => {
+    expect(isPalindrome('dog')).toBe(false);
   });
 });
